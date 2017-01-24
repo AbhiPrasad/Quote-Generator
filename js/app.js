a5d26ba87747d01f1d1353af96dfb907966942c0
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2017-01-24 16:28:41 -0500

    got quote gen working, updating bootstrap

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
-$(document).ready(function() {
+var quoteAPI = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
 
-    var quoteAPI = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
-
-    $('#getquote').click(function() {
-        $.getJSON(quoteAPI).done(update).fail(handleErr);
-    });
-
-    function update(json) {
-        $('#quotetext').html(JSON.stringify(json));
-    }
-
-    function handleErr(jqxhr, textStatus, err) {
-        console.log("Request Failed: " + textStatus + ", " + err);
-    }
-
+$('#getquote').click(function() {
+    $.getJSON(quoteAPI).done(updateQuote).fail(errMsg);
 });
+
+$(document).ready(function() {
+    $.getJSON(quoteAPI).done(updateQuote).fail(errMsg);
+})
+
+function updateQuote(json) {
+    var quoteTxt = json.quoteText.replace(/"/g, "");
+    var quoteAut = json.quoteAuthor.replace(/"/g, "");
+
+    if (quoteAut === "") {
+        quoteAut = "Anonymous";
+    }
+
+    $('#quotetext').html(JSON.stringify(quoteTxt));
+    $('#quoteauthor').html(JSON.stringify(quoteAut));
+}
+
+function errMsg(jqxhr, textStatus, err) {
+    console.log("Request Failed: " + textStatus + ", " + err);
+}
