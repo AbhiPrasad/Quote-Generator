92b4f63954364cfdb452f9b45abf7faf3aac5f4d
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2017-01-24 16:50:20 -0500

    added font awesome and twitter functions

--- js/app.js
+++ js/app.js
@@ -9,8 +9,12 @@ $(document).ready(function() {
 })
 
 function updateQuote(json) {
-    var quoteTxt = json.quoteText.replace(/"/g, "");
-    var quoteAut = json.quoteAuthor.replace(/"/g, "");
+    var quoteTxt = json.quoteText;
+    var quoteAut = json.quoteAuthor;
+
+    var quoteTweet = quoteTxt + "-" + quoteAut;
+
+    gettweetData(quoteTweet);
 
     if (quoteAut === "") {
         quoteAut = "Anonymous";
@@ -23,3 +27,22 @@ function updateQuote(json) {
 function errMsg(jqxhr, textStatus, err) {
     console.log("Request Failed: " + textStatus + ", " + err);
 }
+
+function gettweetData(quote) {
+    $('#tweet').empty();
+
+    var clone = $('.twitter-share-button-template').clone();
+
+    clone.removeAttr("style"); // unhide the clone
+    // clone.attr("data-url", someDynamicUrl);
+    // clone.attr("data-counturl", someDynamicCountUrl);
+    clone.attr("class", "twitter-share-button");
+
+    // copy cloned button into div that we can clear later
+    $('#tweet').append(clone);
+
+    // reload twitter scripts to force them to run, converting a to iframe
+    $.getScript("http://platform.twitter.com/widgets.js");
+}
+
+//  <a href="http://twitter.com/share" class="twitter-share-button" data-text="This is what we want to change dynamically" data-count="none" data-via="chris_camps">Tweet</a>
